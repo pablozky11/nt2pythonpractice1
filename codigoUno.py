bc16328727fb39c0639d5d91c663c0fa69c7d297
alpine18 <127502255+alpine18@users.noreply.github.com>
2023-08-11 10:26:33 -0500

    nt2 se inicializa proyecto recibiendo variables por consola

---
+++ codigoUno.py
@@ -0,0 +1,4 @@
+nombreUsuario = input ("Ingresar el nombre")
+direccion = input ("Ingresa la direccion")
+edad = int(input ("edad del usuario"))
+print("Nombre: ", nombreUsuario, "tu direccion es: ", direccion, "edad: ", edad)  # imprime en pantalla el valor
